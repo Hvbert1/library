e3189d0cb1d55a102201f10862420231460589ba
Hvbert1 <henryjiafu@gmail.com>
2023-01-17 11:42:21 +1100

    add checkbox

--- script.js
+++ script.js
@@ -19,23 +19,32 @@ function addBookToLibrary() {
 function display() {
     for (var i = 0; i < myLibrary.length; i++) {
         const card = document.createElement("div");
-        card.className = "card";
-
+        const actions = document.createElement("div");
+        const check = document.createElement("div");
         const cardTitle = document.createElement("div");
         const cardAuthor = document.createElement("div");
         const cardPages = document.createElement("div");
         const remove = document.createElement('button');
+        const label = document.createElement('label');
+        const read = document.createElement("input");
 
+        card.className = "card";
+        actions.className = "actions";
         cardTitle.className = "cardTitle";
         cardAuthor.className = "cardAuthor"
         cardPages.className = "cardPages";
+        check.className = "check";
+    
         remove.id = i;
+        read.setAttribute("type", "checkbox");
+        read.id = "read"
+        label.htmlFor = "read";
 
         cardTitle.innerHTML = myLibrary[i].title;
         cardAuthor.innerHTML = myLibrary[i].author;
         cardPages.innerHTML = myLibrary[i].pages + " pages";
         remove.innerHTML = "x";
-
+        label.innerHTML = "Read?";
 
         remove.addEventListener('click',function(){ 
             if (this.id > -1) {
@@ -49,7 +58,12 @@ function display() {
         document.getElementsByClassName("card")[i].appendChild(cardTitle);
         document.getElementsByClassName("card")[i].appendChild(cardAuthor);
         document.getElementsByClassName("card")[i].appendChild(cardPages);
-        document.getElementsByClassName("card")[i].appendChild(remove);
+
+        document.getElementsByClassName("card")[i].appendChild(actions);
+        document.getElementsByClassName("actions")[i].appendChild(check);
+        document.getElementsByClassName("check")[i].appendChild(label);
+        document.getElementsByClassName("check")[i].appendChild(read);
+        document.getElementsByClassName("actions")[i].appendChild(remove);
     }
 }
 
